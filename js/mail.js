a7cf1460845075cdb3916d960eff905c7811b064
Cornee5674 <corneeruiter@gmail.com>
2020-05-28 10:14:57 +0200

    Responsiveness hotfix

--- js/mail.js
+++
@@ -1,9 +0,0 @@
-
-
-function sendMail(){
-    const email = document.getElementById("email").value;
-    const name = document.getElementById("name").value;
-    const subject = document.getElementById("subject").value;
-    const body = document.getElementById("body").value;
-    window.open('mailto:corneeruiter@gmail.com?subject='+ subject + '&body=' + body + ",     naam: " + name + ",      email: " + email);
-}
